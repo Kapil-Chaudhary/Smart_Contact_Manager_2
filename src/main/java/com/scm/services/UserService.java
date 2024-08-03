24f43fb60de9441b094800297aeedea663d8597e
Kapil Chaudhary <kapilchaudhary024@gmail.com>
2024-08-03 17:47:17 +0530

    1. fixing all bugs upto lacture 18 2. fixing userRepo, userService and userServiceImpl 3. here problem is userService extends userRepo which is not right

--- src/main/java/com/scm/services/UserService.java
+++ src/main/java/com/scm/services/UserService.java
@@ -10,16 +10,23 @@ import java.util.List;
 import java.util.Optional;
 
 
-public interface UserService extends UserRepo {
+public interface UserService {
 
     User saveUser(User user);
+
     Optional<User> getUserById(String id);
+
     Optional<User> updateUser(User user);
+
     void deleteUser(String id);
+
     boolean isUserExists(String id);
+
     boolean isUserExistsByEmail(String email);
 
     List<User> getAllUsers();
+
+    User getUserByEmail(String email);
 
 
     // add more user methods here related user service[logic]
